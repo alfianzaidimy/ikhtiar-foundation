319a3743208759610aa265307e08ba0f3fd9c8a0
alfianzaidimy <alfianzaidi@gmail.com>
2026-08-06 14:20:06 +0800

    Update page.js

--- app/pasukan/page.js
+++ app/pasukan/page.js
@@ -77,12 +77,12 @@ export default function PasukanPage() {
               key={member.id} 
               className="group relative bg-gradient-to-b from-red-950 to-zinc-950 rounded-2xl overflow-hidden shadow-md border border-zinc-800/50 aspect-[3/4] flex flex-col justify-end transition-transform duration-300 hover:scale-[1.02] hover:shadow-xl"
             >
-              {/* Watermark Logo Kecil di Penjuru Atas Kanan */}
-              <div className="absolute top-4 right-4 z-20 select-none opacity-40 group-hover:opacity-80 transition-opacity">
+              {/* Watermark Logo Baharu di Penjuru Atas Kanan */}
+              <div className="absolute top-4 right-4 z-20 select-none opacity-50 group-hover:opacity-90 transition-opacity">
                 <img 
-                  src="/edit.png" 
+                  src="/smallllogo.png" 
                   alt="Logo Watermark" 
-                  className="w-7 h-7 object-contain filter brightness-200" 
+                  className="w-7 h-7 object-contain" 
                 />
               </div>
 
